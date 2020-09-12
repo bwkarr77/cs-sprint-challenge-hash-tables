553d100d97d08468769ca4732fdbef61cbffd5b2
Brett Karr <b.karr@polyclip.us>
2020-09-12 11:51:23 -0500

    ex2 passing tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,20 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = []
+    for each in tickets:
+        source = each.source
+        # if source = "NONE", then it's the start of the trip
+        cache[source] = each.destination
+    # starting/ending location = "NONE" (aka start/stop the trip)
+    board = cache["NONE"]
 
+    while board is not "NONE":
+        # start with 'NONE', and append each trip afterwards
+        route.append(board)
+        # set the destination of the current flight to be the key of next flight
+        board = cache[board]
+        # the LAST flight will have 'NONE' as it's destination
+    route.append(board)
     return route
