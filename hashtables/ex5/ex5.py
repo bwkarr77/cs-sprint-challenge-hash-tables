3bfe0c06eb1a793635fb91884ecad7893ef643fa
Brett Karr <b.karr@polyclip.us>
2020-09-12 13:44:36 -0500

    all 5 exercises completed and passing all tests!

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,15 +1,24 @@
-# Your code here
-
-
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+    for file in files:
+        filename = file.split('/')[-1]
+        if filename in cache:
+            cache[filename].append(file)
+        else:
+            cache[filename] = [file]
 
+    for query in queries:
+        if query in cache:
+            # loop through EACH filename at the cache with query name:
+            for each in cache[query]:
+                result.append(each)
     return result
-
 
 if __name__ == "__main__":
     files = [
